a04a3794e0fb50a39100643b7dca0c7e4fde72ff
AndrewH <andrewrhands@gmail.com>
2017-10-21 18:10:30 -0500

    worked on having multiple elements on a single graph

---
+++ email_analyzer/ParsedPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace email_analyzer
+{
+    class ParsedPackage
+    {
+        public ParsedPackage(string from,string subject,string body, DateTimeOffset timeDelivered)
+        {
+            From = from;
+            Subject = subject;
+            Body = body;
+            TimeDelivered = timeDelivered;
+        }
+        public string From { get; }
+        public string Subject { get; }
+        public string Body { get; }
+        public DateTimeOffset TimeDelivered { get; }
+    }
+}
